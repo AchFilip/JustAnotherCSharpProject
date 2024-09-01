0ae151de25cf3f4b8c5f92e0d55aa0c4e0b5e5c4
GURU\grgur <grguru99@gmail.com>
2024-09-01 20:17:53 +0300

    - Completed Task 1. - Removed a couple of unused requests - Cleanup the Task 1 endpoint.

--- Data/ProjectNoviContext.cs
+++ Data/ProjectNoviContext.cs
@@ -11,20 +11,4 @@ public class ProjectNoviContext(DbContextOptions<ProjectNoviContext> options)
     public DbSet<IP> Ips => Set<IP>();
 
     public DbSet<Country> Countries => Set<Country>();
-
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
-    //     //Create Migrations
-    //     //dotnet ef migrations add InitialCreate --output-dir Data/Migrations
-
-    //     //Generate DB
-    //     //dotnet ef database update
-
-    //     //Seed Data.
-    //     //dotnet ef migrations add SeedGenres --output-dir Data/Migrations
-    //     modelBuilder.Entity<Country>().HasData(
-    //         new {Id = 1, CountryName= "Greece", TwoLetterCode = "GR", ThreeLetterCode = "GRC"}
-    //     );
-
-    // }
 }
--- EndPoints/IpInfoEndpoints.cs
+++ EndPoints/IpInfoEndpoints.cs
@@ -10,28 +10,20 @@ namespace ProjectNovi.Api.Endpoints;
 
 public static class IpInfoEndpoints
 {
-    private static readonly List<IpInfoDto> IpInfos = [
-
-    ];
-
     public static RouteGroupBuilder MapIpInfoEndpoints(this WebApplication app)
     {
-
-
         var group = app.MapGroup("ip");
-
-        //GET /ip
-        group.MapGet("/", () => IpInfos);
 
         //GET /ip/dbs 
         //Receives data from db.
-        group.MapGet("/dbs", (ProjectNoviContext dbContext) => 
+        group.MapGet("/dbs", (ProjectNoviContext dbContext) =>
         {
             var ips = dbContext.Ips.ToList();
 
             List<IpInfoDto> results = [];
 
-            for(int i = 0 ; i < ips.Count; i++){
+            for (int i = 0; i < ips.Count; i++)
+            {
                 results.Add(new IpInfoDto
                 (
                     ips[i].Id,
@@ -41,152 +33,36 @@ public static class IpInfoEndpoints
                     ips[i].Country.ThreeLetterCode
                 ));
             }
-            
+
             return Results.Ok(ips);
         });
 
         //Task 1
         group.MapGet("/{ipAddress}", async (string ipAddress, ProjectNoviContext dbContext, IMemoryCache cache) =>
         {
-
-            //Check Cache
-            var cacheKey = $"IpInfo_{ipAddress}";
             // Check the cache first
+            var cacheKey = $"IpInfo_{ipAddress}";
             if (cache.TryGetValue(cacheKey, out IpInfoDto cachedIpInfo))
             {
                 return Results.Ok(cachedIpInfo + " in Cache");
             }
 
-            //Check DB
-            //Get the ips from the db.
-            var ip = dbContext.Ips
-                    .Include(ip => ip.Country)
-                    .Where(ip => ip.IpAddress == ipAddress);
+            //Check Database
+            var ip = await dbContext.Ips
+                    .Include(ip => ip.Country)        //Include is not that optimized.
+                    .Where(ip => ip.IpAddress == ipAddress)
+                    .FirstOrDefaultAsync();
 
-            //Check if is not empty
-            if(ip.Any()){
-                IpInfoDto tett = new(
-                    1,
-                    "122.222.22.222",
-                    "Japan",
-                    "JP",
-                    "JPN"
-                );
-
-                cache.Set(cacheKey, tett, TimeSpan.FromHours(1)); // Cache for 1 hour
-                return Results.Ok("To VRika alla dn boro na to printaro gamw" + ip.ToList() + " in DB");
+            if (ip != null)
+            {
+                cache.Set(cacheKey, ip, TimeSpan.FromHours(1)); // Cache for 1 hour
+                return Results.Ok(ip + " in DB");
             }
 
-            //if not in db CheckIp2C
-            //Save to DB and Cache and return
-
-            
-                using var httpClient = new HttpClient();
-
-                // Build the URL with the provided IP address
-                string url = $"http://ip2c.org/{ipAddress}";
-
-                // Send a GET request to the external service
-                HttpResponseMessage response = await httpClient.GetAsync(url);
-
-                // Ensure the request was successful
-                if (response.IsSuccessStatusCode)
-                {
-                    // Read the response content as a string
-                    string content = await response.Content.ReadAsStringAsync();
-
-                    // Split the string by semicolon
-                    string[] result = content.Split(';');
-    
-                    IpInfos.Add(new IpInfoDto(
-                        IpInfos.Count + 1,
-                        ipAddress,
-                        result[3],
-                        result[1],
-                        result[2]
-                    ));
-
-
-                    // Create a new Country and IP entity
-                    var country = new Country
-                    {
-                        Id = IpInfos.Count,
-                        CountryName = result[3],
-                        TwoLetterCode = result[1],
-                        ThreeLetterCode = result[2]
-                    };
-
-                    var ipEntity = new IP
-                    {
-                        Id = IpInfos.Count,
-                        IpAddress = ipAddress,
-                        CountryId = dbContext.Countries.Count() + 1,
-                        Country = country
-                    };
-
-                    // Save to the database
-                    dbContext.Add(ipEntity);
-                    await dbContext.SaveChangesAsync();
-
-                    cache.Set(cacheKey, IpInfos[IpInfos.Count-1], TimeSpan.FromHours(1)); // Cache for 1 hour
-
-                    // Return the content as the response
-                    return Results.Ok(IpInfos[IpInfos.Count-1] + " Found in IP2C");
-                }
-                else
-                {
-                    // Return a failure status if the request failed
-                    return Results.StatusCode((int)response.StatusCode);
-                }
-        });
-
-        //Task 3
-        group.MapGet("/sql/{twolettercode}", (string[]? twolettercode, ProjectNoviContext dbContext) => 
-        {
-                //If null return everything
-                if (twolettercode == null)
-                {
-                    var allCountriesQuery = @"
-                        SELECT c.CountryName, 
-                            COUNT(ip.Id) AS AddressesCount
-                        FROM Countries c
-                        LEFT JOIN IPs ip ON ip.CountryId = c.Id
-                        GROUP BY c.CountryName";
-
-                    var allCountries = dbContext.Countries
-                                    .FromSqlRaw(allCountriesQuery)
-                                    .ToList();
-
-                    return Results.Ok(allCountries + " From Null");
-                }
-
-                if(twolettercode.Length == 0){
-                    return Results.NotFound($"You did not gave any countries");
-                }
-
-                //Get the ips from the db.
-                var ips = dbContext.Ips.ToList();
-
-                for(int i = 0 ; i<twolettercode.Length; i++){
-                    var code = twolettercode[i];
-                    var country = dbContext.Countries
-                                .Where(c => c.TwoLetterCode == code);
-
-                    if(country is not null)
-                        return Results.Ok(country);
-                }
-
-                return Results.Ok(twolettercode);
-        });
-
-        group.MapGet("/ip2c/{ipAddress}", async (string ipAddress) =>
-        {
+            //If not in Database, fetch from IP2C
             using var httpClient = new HttpClient();
 
-            // Build the URL with the provided IP address
             string url = $"http://ip2c.org/{ipAddress}";
-
-            // Send a GET request to the external service
             HttpResponseMessage response = await httpClient.GetAsync(url);
 
             // Ensure the request was successful
@@ -198,16 +74,39 @@ public static class IpInfoEndpoints
                 // Split the string by semicolon
                 string[] result = content.Split(';');
 
-                IpInfos.Add(new IpInfoDto(
-                    IpInfos.Count + 1,
+                int dbLength = await dbContext.Ips.CountAsync(); 
+                IpInfoDto IpInfos = new IpInfoDto(
+                    dbLength + 1,
                     ipAddress,
                     result[3],
                     result[1],
                     result[2]
-                ));
+                );
+
+                var country = new Country
+                {
+                    Id = IpInfos.Id,
+                    CountryName = result[3],
+                    TwoLetterCode = result[1],
+                    ThreeLetterCode = result[2]
+                };
+
+                var ipEntity = new IP
+                {
+                    Id = IpInfos.Id,
+                    IpAddress = ipAddress,
+                    CountryId = dbContext.Countries.Count() + 1,
+                    Country = country
+                };
+
+                // Save to the database
+                dbContext.Add(ipEntity);
+                await dbContext.SaveChangesAsync();
+
+                cache.Set(cacheKey, IpInfos, TimeSpan.FromHours(1)); // Cache for 1 hour
 
                 // Return the content as the response
-                return Results.Ok(result);
+                return Results.Ok(IpInfos + " Found in IP2C");
             }
             else
             {
@@ -216,7 +115,50 @@ public static class IpInfoEndpoints
             }
         });
 
+        //Task 3
+        //TODO: EntityFramework -> Raw SQL
+        //TODO: Create a .csv report
+        //TODO: Add last edited time
+        group.MapGet("/sql/{twolettercode}", (string[]? twolettercode, ProjectNoviContext dbContext) =>
+        {
+            //If null return everything
+            if (twolettercode[0] == "null")
+            {
+                var allCountriesQuery = @"
+                        SELECT c.CountryName, 
+                            COUNT(ip.Id) AS AddressesCount
+                        FROM Countries c
+                        LEFT JOIN IPs ip ON ip.CountryId = c.Id
+                        GROUP BY c.CountryName";
 
+                var allCountries = dbContext.Countries
+                                .FromSqlRaw(allCountriesQuery)
+                                .ToList();
+
+                return Results.Ok(allCountries + " From Null");
+            }
+
+            if (twolettercode.Length == 0)
+            {
+                return Results.NotFound($"You did not gave any countries");
+            }
+
+            //TODO: Change entityframework to raw sql
+            //Get the ips from the db.
+            var ips = dbContext.Ips.ToList();
+
+            for (int i = 0; i < twolettercode.Length; i++)
+            {
+                var code = twolettercode[i];
+                var country = dbContext.Countries
+                            .Where(c => c.TwoLetterCode == code);
+
+                if (country is not null)
+                    return Results.Ok(country);
+            }
+
+            return Results.Ok(twolettercode);
+        });
 
         return group;
     }
